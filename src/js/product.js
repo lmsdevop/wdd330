0df52ae497a634677ed34bcb96d73c147edb7773
Lucas Mendonça <lms.devop@gmail.com>
2024-07-01 17:26:03 -0300

    Initial commit

---
+++ src/js/product.js
@@ -0,0 +1,25 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+
+const dataSource = new ProductData("tents");
+
+function addProductToCart(product) {
+  let cartItems = getLocalStorage("so-cart");
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
+  // Adiciona o novo produto ao array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
+}
+
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
